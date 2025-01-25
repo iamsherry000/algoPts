a98621d3010fe1b1df84bdc0d5fe62103f4a4c95
iamsherry000 <iamsherry000@gmail.com>
2025-01-25 12:56:26 +0800

    Add book

---
+++ algo/hash/hash.java
@@ -0,0 +1,29 @@
+package algo.hash;
+
+import java.util.LinkedList;
+
+public class hash {
+
+    public void closedAddressing() {
+        private LinkedList<Entry>[] table;
+        private int capacity;
+
+        class Entry {
+            String key;
+            int value;
+
+            Entry(String key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+        
+        
+    }
+    
+    public void openAddressing() {
+
+    }
+
+    
+}
