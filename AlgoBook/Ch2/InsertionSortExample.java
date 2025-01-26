157a2885038680d9283dd3161864c25841679f19
iamsherry000 <iamsherry000@gmail.com>
2025-01-26 21:33:38 +0800

    Practice

---
+++ AlgoBook/Ch2/InsertionSortExample.java
@@ -0,0 +1,40 @@
+/*
+ * Insertion Sort (p 18.)
+ * An efficient algorithm for sorting a small number of element, 
+ * for example: playing cards, empty left hand and remove one card at a time from the table 
+ * and insert it into the correct position in the left hand.
+ */
+
+package AlgoBook.Ch2;
+
+import java.util.Arrays;
+
+public class InsertionSortExample {
+    public static void main(String[] args) {
+        InsertionSortExample example = new InsertionSortExample();
+        example.runTestCases();
+    }
+
+    public void runTestCases() {
+        int[] A = {5, 2, 4, 6, 1, 3};
+        System.out.println("排序前: " + Arrays.toString(A));
+        insertionSort(A, true);
+        System.out.println("小到大排序: " + Arrays.toString(A));
+        insertionSort(A, false);
+        System.out.println("大到小排序: " + Arrays.toString(A));
+    }
+
+    private void insertionSort(int[] array, boolean ascending) {
+        for(int j = 0 ; j < array.length; j++) {
+            int key = array[j]; 
+            int i = j - 1;
+
+            // ascending true : 小到大排, array[i] > key, 大的向右移動
+            while(i >= 0 && (ascending ? array[i] > key : array[i] < key)) { 
+                array[i+1] = array[i];
+                i = i - 1;
+            }
+            array[i + 1] = key; 
+        }
+    }
+}
---
+++ AlgoBook/Ch2/LinearSearchExample.java
@@ -0,0 +1,27 @@
+/*
+ * p 22. 2.1-3 exercises.
+ */
+package AlgoBook.Ch2;
+
+public class LinearSearchExample {
+    public static void main(String[] args) {
+        LinearSearchExample example = new LinearSearchExample();
+        example.runTestCases();
+    }
+
+    public void runTestCases() {
+        int[] A = {5, 2, 4, 6, 1, 3};
+        int v = 4;
+        int result = linearSearch(A, v);
+        System.out.println("Search: " + v + " index: " + (result != -1 ? result : "NIL"));
+    }
+
+    private static int linearSearch(int[] array, int v) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == v) {
+                return i; // 找到則返回索引
+            }
+        }
+        return -1; // 未找到返回 -1 作為 NIL
+    }
+}
